fd82c16bb6639fc02dd140e00a4d516fc10cd60e
BK <birisesports@gmail.com>
2025-12-02 21:06:49 -0300

    feat: implement file and directory creation methods in Scaffolder

---
+++ internal/scaffolder/file.go
@@ -0,0 +1,53 @@
+package scaffolder
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type FileTemplate struct {
+	Path        string
+	Content     string
+	Permissions os.FileMode
+}
+
+func (s *Scaffolder) createFileFromTemplate(ft FileTemplate, vars *TemplateVars) error {
+	processedPath, err := s.processPathTemplate(ft.Path, vars)
+	if err != nil {
+		return fmt.Errorf("failed to process path template: %w", err)
+	}
+
+	fullPath := s.joinPath(processedPath)
+
+	dir := filepath.Dir(fullPath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("failed to create directory: %w", err)
+	}
+
+	processedContent, err := s.processTemplate(ft.Content, vars)
+	if err != nil {
+		return fmt.Errorf("failed to process content template: %w", err)
+	}
+
+	perms := ft.Permissions
+	if perms == 0 {
+		perms = 0644
+	}
+
+	if err := os.WriteFile(fullPath, []byte(processedContent), perms); err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+
+	return nil
+}
+
+func (s *Scaffolder) createDirectory(path string, vars *TemplateVars) error {
+	processedPath, err := s.processPathTemplate(path, vars)
+	if err != nil {
+		return fmt.Errorf("failed to process path template: %w", err)
+	}
+
+	fullPath := s.joinPath(processedPath)
+	return os.MkdirAll(fullPath, 0755)
+}
